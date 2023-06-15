bba48b0acf8404755c3706a5cc7a208e1ef9a866
asepwahyudi <asepwahyudi1501@gmail.com>
2023-06-15 15:42:19 +0700

    menghapus script app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
-<script setup>
-import { ref, onMounted } from "vue";
-import MusicPlayer from "./components/MusicPlayer.vue";
-
-import { useSongStore } from "./stores/song";
-import { storeToRefs } from "pinia";
-const useSong = useSongStore();
-const { isPlaying, currentTrack } = storeToRefs(useSong);
-
-onMounted(() => {
-  isPlaying.value = false;
-});
-</script>
-
 <template>
   <main>
     <router-view />
